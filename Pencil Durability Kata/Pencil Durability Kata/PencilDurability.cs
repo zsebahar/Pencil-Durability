d12927f98010d2c469ccc5546cfc14bbaf5faa1a
Zack <zsebahar@gmail.com>
2019-08-26 19:57:25 -0500

    Test to verify a durability of 1 can write a single character

---
+++ Pencil Durability Kata/Pencil Durability Kata/PencilDurability.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pencil_Durability_Kata
+{
+    internal class PencilDurability
+    {
+        private int v;
+
+        public PencilDurability(int v)
+        {
+            this.v = v;
+        }
+
+        public string Write(string v)
+        {
+            return v;
+        }
+    }
+}
--- Pencil Durability Kata/Pencil Durability Kata/PencilDurabilityTest.cs
+++ Pencil Durability Kata/Pencil Durability Kata/PencilDurabilityTest.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using Xunit;
 
 namespace Pencil_Durability_Kata
 {
     public class PencilDurabilityTest
     {
+        [Fact]
+        public void WhenDurabilityIsOneAndPencilDurabilityWritesOneLetterReturnSameLetter()
+        {
+            PencilDurability pencilDurability = new PencilDurability(1);
+            string result = pencilDurability.Write("t");
+            Assert.Equal("t", result);
+        }
     }
 }
